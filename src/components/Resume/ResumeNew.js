860ff599e29a5bf0adf7583454fc740e008d37e8
Krish Rathod <2001krish.rathod@gmail.com>
2022-07-08 16:36:02 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://src/Assets/krishrathod_resume.pdf";
+  "https://rawgithubusercontent.com/KrishRathod/Portfolio-Website/master/src/Assets/krishrathod_resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
